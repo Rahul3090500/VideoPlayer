44980b2dbfedbbf84349a294d209f7d54ad40494
Savannah Niles <savannahniles@gmail.com>
2014-06-05 18:04:21 -0400

    ch-ch-ch-changes

--- js/um-video-player.js
+++ js/um-video-player.js
@@ -1,17 +1,26 @@
-function UMVideoPlayer(videoId, wrapperId, onReady, onLoadError, onRenderObjectTimeUpdate, onVideoFinish, transition) {
+function UMVideoPlayer(wrapperId, renderObject, options) {
+    //to note: clips 1 sec or less experience a delay, need to optimize.
+    //option to load X number of videos in advance to adjust accordingly
+
     var self = this;
 
     this.videoContainer = document.getElementById(wrapperId);
-    console.log (self.videoContainer);
-    this.videoId = videoId;
-    this.videoStyle;//document.getElementById(videoId).style;
-    this.placeholderHeight = 0;
-    this.placeholderWidth = 0;
 
-    this.onReady = onReady;
-    this.onLoadError = onLoadError;
-    this.onRenderObjectTimeUpdate = onRenderObjectTimeUpdate;
-    this.onVideoFinish = onVideoFinish;
+    //options
+    this.onReady = options.onReady;
+    this.onLoadError = options.onLoadError;
+    this.onRenderObjectTimeUpdate = options.onRenderObjectTimeUpdate;
+    this.onVideoFinish = options.onVideoFinish;
+
+    this.transitionTime = 0;
+    if (options.transitionTime != null && typeof options.transitionTime == "number") {
+        this.transitionTime = options.transitionTime;
+    }
+
+    this.classString = ""; //option= additional class tags
+    if (options.classString != null && typeof options.classString == "string") {
+        this.classString = options.classString;
+    }
 
     this.renderObj = null;
     this.currentVideo = 0;
@@ -21,29 +30,12 @@ function UMVideoPlayer(videoId, wrapperId, onReady, onLoadError, onRenderObjectT
     this.contentTime = [];
     this.isVideoPlaying = false;
 
-    this.transitionTime = 300;
-    if (transition != null && typeof transition == "number") {
-        this.transitionTime = transition;
-    }
-
-    this.setRenderObject = function(obj) {
-
-        this.renderObj = obj;
-
-        if (this.renderObj == null) {
-            this.onLoadError("Invalid Render Object");
-            return;
-        }
-
-        this.loadInitialVideo();
-        return true;
-    }
+    //--------------------------------------------------
 
     this.play = function () {
         if (this.isVideoReady) {
-            var videoElement = document.getElementById("video-" + self.videoUuid + "-" + self.currentVideo);//.getAttribute("id");
+            var videoElement = document.getElementById("video-" + self.elementId + "-" + self.currentVideo);//.getAttribute("id");
             console.log(videoElement);
-            videoElement.style.webkitTransition = "opacity .3s"; //may want to move
             videoElement.style.opacity = "1";
             this.videoObjects[self.currentVideo].play();
             this.isVideoPlaying = true;
@@ -88,54 +80,53 @@ function UMVideoPlayer(videoId, wrapperId, onReady, onLoadError, onRenderObjectT
 
     this.loadInitialVideo = function () {
 
-        //console.log("loadInitialVideo");
-        //this is all fishy 
-
         this.currentVideo = 0;
-        this.videoObjects = new Array(this.renderObj.contentURLs.length);
         this.isVideoReady = false;
-        this.videoUuid = self.generateId(10);
-        this.renderObjectTime = 0;
-        this.contentTime = new Array(this.renderObj.contentURLs.length);
-        // this.placeholderWidth = document.querySelector('#'+this.placeholderDiv).clientWidth();
-        // this.placeholderHeight = document.querySelector('#'+this.placeholderDiv).clientHeight();
+        this.elementId = self.generateId(10);
+        // this.renderObjectTime = 0;
+        this.contentTime = new Array(this.renderObj.EDL.length);
 
         this.loadVideoElement(0);
     }
 
     this.loadVideoElement = function (id) {
-
-        //contentURLs = EDL
         
-        var content = this.renderObj.contentURLs[id];
-        this.appendVideo(id, content.url, content.startTime, content.endTime); //keep this don't touch
+        var content = this.renderObj.EDL[id];
+        this.appendVideo(id, content.url, content.startTime, content.endTime); 
 
-        if (this.renderObj.contentURLs.length > id + 1) {
+        if (this.renderObj.EDL.length > id + 1) {
             content = null;
-            content = this.renderObj.contentURLs[id + 1];
+            content = this.renderObj.EDL[id + 1];
             this.appendVideo(id + 1, content.url, content.startTime, content.endTime);
         }
     }
 
     this.appendVideo = function (id) {
 
-        var content = this.renderObj.contentURLs[id];
-        var classTag = null; //option= additional class tags
+        var content = this.renderObj.EDL[id];
 
         var videoElement = document.createElement('video');
-        videoElement.setAttribute('id', "video-" + self.videoUuid + "-" + id);
-        videoElement.setAttribute('class', "um_video_player vjs-default-skin");
-        videoElement.setAttribute('height', 400);
-        videoElement.setAttribute('width', 400);
-        videoElement.setAttribute('src', content.url + "#t=" + content.startTime + "," + content.endTime);
+        videoElement.setAttribute('id', "video-" + self.elementId + "-" + id);
+        videoElement.setAttribute('class', self.classString);
+
+        var timeString = "#t=";
+        timeString += content.startTime;
+        if (content.endTime)
+        {
+            timeString += "," + content.endTime;
+        }
+        if (timeString == "#t=")
+        {
+            timeString = "";
+        }
+        videoElement.setAttribute('src', content.url + timeString);
+
         videoElement.setAttribute('controls', true);
         videoElement.setAttribute('preload', "auto");
 
         videoElement.style.position = "absolute";
-        videoElement.style.left = "0px";
-        videoElement.style.top = "0px";
         videoElement.style.opacity = "0";
-        videoElement.style.webkitTransitionEnd = ".3s";
+        videoElement.style.webkitTransition = self.transitionTime + "s";
 
         videoElement.addEventListener("loadedmetadata", self.onMetadataLoaded);
         videoElement.addEventListener("loadeddata", self.onVideoReady);
@@ -152,22 +143,22 @@ function UMVideoPlayer(videoId, wrapperId, onReady, onLoadError, onRenderObjectT
     this.onMetadataLoaded = function() {
                 
         var elementId = this.id;
-        var videoId = parseInt(elementId.replace("video-" + self.videoUuid + "-", ""));
+        var videoId = parseInt(elementId.replace("video-" + self.elementId + "-", ""));
 
         if (videoId == null || videoId < 0) {
             self.onLoadError("Something wrong with video element...");
             return;
         }
 
-        self.currentTime(self.renderObj.contentURLs[videoId].startTime);
-        self.contentTime[videoId] = self.renderObj.contentURLs[videoId].startTime;
+        self.currentTime(self.renderObj.EDL[videoId].startTime);
+        self.contentTime[videoId] = self.renderObj.EDL[videoId].startTime;
 
     }
 
     this.onVideoReady = function() {
 
         var elementId = this.id;
-        var videoId = parseInt(elementId.replace("video-" + self.videoUuid + "-", ""));
+        var videoId = parseInt(elementId.replace("video-" + self.elementId + "-", ""));
 
         if (videoId == null || videoId < 0) {
             self.onLoadError("Something wrong with video element...");
@@ -176,6 +167,7 @@ function UMVideoPlayer(videoId, wrapperId, onReady, onLoadError, onRenderObjectT
 
         if (videoId == 0) {
             self.isVideoReady = true;
+            document.getElementById("video-" + self.elementId + "-" + self.currentVideo).style.opacity = "1";
             self.onReady();
         }
     }
@@ -186,20 +178,18 @@ function UMVideoPlayer(videoId, wrapperId, onReady, onLoadError, onRenderObjectT
     }
 
     this.onPause = function() {
-        console.log("onPause");
-        console.log ("currentVideo = " + self.currentVideo); 
-        console.log ("self.renderObj.contentURLs.length= " + self.renderObj.contentURLs.length);
-        console.log ("self.renderObj.contentURLs[self.currentVideo].endTime= " + self.renderObj.contentURLs[self.currentVideo].endTime); 
-        console.log ("this.currentTime = " + self.currentTime());
-        console.log ("video currentTime = " + document.getElementById("video-" + self.videoUuid + "-" + self.currentVideo).currentTime);
+        // console.log("onPause");
+        // console.log ("currentVideo = " + self.currentVideo); 
+        // console.log ("self.renderObj.EDL.length= " + self.renderObj.EDL.length);
+        // console.log ("self.renderObj.EDL[self.currentVideo].endTime= " + self.renderObj.EDL[self.currentVideo].endTime); 
+        // console.log ("this.currentTime = " + self.currentTime());
+        // console.log ("video currentTime = " + document.getElementById("video-" + self.elementId + "-" + self.currentVideo).currentTime);
 
-        var videoElement = document.getElementById("video-" + self.videoUuid + "-" + self.currentVideo);
-        if (self.renderObj.contentURLs[self.currentVideo].endTime <= videoElement.currentTime) {
-            console.log("Switch!");
+        var videoElement = document.getElementById("video-" + self.elementId + "-" + self.currentVideo);
+        if (self.renderObj.EDL[self.currentVideo].endTime <= videoElement.currentTime) {
  
             self.currentVideo++;
 
-            console.log ("Boom." + self.currentVideo);
             if (self.currentVideo - 1 >= 0) {
                     //console.log("video being paused", self.currentVideo - 1);
                     if (self.videoObjects[self.currentVideo - 1] != null) {
@@ -207,29 +197,27 @@ function UMVideoPlayer(videoId, wrapperId, onReady, onLoadError, onRenderObjectT
                     }
                     self.videoObjects[self.currentVideo - 1] = null;
                 }
-            console.log ("Hiding" + self.currentVideo);
             this.remove();
             videoElement.style.opacity = "0"; 
 
 
-            if (self.renderObj.contentURLs.length > self.currentVideo) {
-                console.log ("FUCK HERE." + self.currentVideo);
-                var videoElement2 = document.querySelector("#video-" + self.videoUuid + "-" + self.currentVideo);
+            if (self.renderObj.EDL.length > self.currentVideo) {
+                var videoElement2 = document.querySelector("#video-" + self.elementId + "-" + self.currentVideo);
                 videoElement2.style.opacity = "1";
                 self.videoObjects[self.currentVideo].play();
 
-                if (self.renderObj.contentURLs.length > self.currentVideo + 1) {
-                    var content = self.renderObj.contentURLs[self.currentVideo + 1];
+                if (self.renderObj.EDL.length > self.currentVideo + 1) {
+                    var content = self.renderObj.EDL[self.currentVideo + 1];
                     self.appendVideo(self.currentVideo + 1, content.url, content.startTime, content.endTime);
                 }
 
             }
+            else {
+                // self.onVideoFinish();
+                self.loadInitialVideo();
+                self.isVideoPlaying = false;
+            }
 
-        }
-        if (self.currentVideo == self.renderObj.contentURLs.length && (self.renderObj.contentURLs[self.currentVideo - 1].endTime*1000) - (this.currentTime()*1000) < self.transitionTime) {
-            self.onVideoFinish();    
-            self.loadInitialVideo();
-            self.isVideoPlaying = false;
         }
     }
 
@@ -240,4 +228,17 @@ function UMVideoPlayer(videoId, wrapperId, onReady, onLoadError, onRenderObjectT
         //fullscreen support
         //better controls
     }
+
+    //--------------------------------------------------
+
+    this.renderObj = renderObject;
+
+    if (this.renderObj == null) {
+        this.onLoadError("Invalid Render Object");
+        return;
+    }
+
+    this.loadInitialVideo();
+
+    //--------------------------------------------------
 }
